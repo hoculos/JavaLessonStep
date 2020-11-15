6a50b760bc9137e501649ad12994382d8bc3af71
Eugne Ugaev <hoculos@gmail.com>
2020-11-15 20:00:04 +0200

    Example overriding

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -3,39 +3,19 @@
  *
  * @author UgaevEugen
  */
-import LessonThreeStep.HorizontalLine;
-import LessonThreeStep.SumOddIntegers;
-import LessonThreeStep.factorial;
-import LessonFourStep.ReverseNumber;
-import LessonSixStep.ArrayOne;
+
+
+import overriding.car;
+import overriding.BMW;
+
 
 public class main {
 
     public static void main(String[] agrs) {
 
-        //ArrayOne foo = new ArrayOne(); 
-        //foo.ShowArray();
-        int mass[] = {1, 2, 3, 6, 10};
-        int SumEven = 0;
-        int SumNotEven = 0;
-
-        boolean SumEvenBool = true;
-        boolean SumNotEvenBool = true;
-
-        for (int i = 0; i < mass.length; i++) {
-
-            SumEvenBool = (mass[i] % 2 == 0);
-            if (SumEvenBool) {
-                SumEven = SumEven + mass[i];
-            }
-
-            SumNotEvenBool = (mass[i] % 2 == 1);
-            if (SumNotEvenBool) {
-                SumNotEven = SumNotEven + mass[i]; 
-            }
-        }
-
-        System.out.println(SumEven);
-        System.out.print(SumNotEven);
+       car foo = new BMW("BMW");
+       
+       foo.show();
+       
     }
 }
